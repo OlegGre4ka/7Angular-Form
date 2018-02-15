624fd273f0747667224ec81be5d8799f81cc7438
OlegGre4ka <gre4kae@ukr.net>
2018-02-15 23:48:19 +0300

    TD-formData-isSubmited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,28 @@ import { NgForm } from '@angular/forms';
   // styles:[`input.ng-invalid.ng-touched{border:1px solid red}` ]
 })
 export class AppComponent {
-  //@ViewChild('form') form: NgForm;
+  @ViewChild('form') form: NgForm;
   answers = [{
     type: 'yes',
     text: 'Так'
   }, {
     type: 'no',
     text: 'Ні'
+  },
+  {
+    type: 'no-mean',
+    text: 'Не впевнений'
   }];
+  defaultCountry = 'ua';
+  defaultAnswer = 'no-mean';
 
- // submitForm() {
-  submitForm(form: NgForm) {
-    
-   // console.log('Submited!', this.form);
-    console.log('Submited!', form);
-    
+  formData = {};
+  isSubmited = false;
+ submitForm() {
+ // submitForm(form: NgForm) {
+ this.isSubmited = true;
+ this.formData = this.form.value;
+  console.log('Submited!', this.form);
+    // console.log('Submited!', form);
   }
 }
